742d18f6b6eac51be8c0d6ff32ab67a58c538ee0
brunoroque-sys <bruno.roque@3rcore.com>
2026-01-09 11:22:05 -0500

    agregando hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import ServicesSlider from '@/components/layout/ServicesWheel'
-import TeamSection from '@/components/layout/TeamSection'
-import StatsAndCTA from '@/components/layout/CTASection'
+import ServicesSlider from '@/components/layout/home/ServicesWheel'
+import TeamSection from '@/components/layout/home/TeamSection'
+import StatsAndCTA from '@/components/layout/home/CTASection'
+import Hero from '@/components/layout/home/Hero'
 export default function Home() {
   return (
     <main >
+      <Hero></Hero>
       <TeamSection></TeamSection>
       <ServicesSlider ></ServicesSlider>
       <StatsAndCTA></StatsAndCTA>
