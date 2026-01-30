54395afec981bc935160e806301ea09570abd393
brunoroque-sys <bruno.roque@3rcore.com>
2026-01-30 12:25:02 -0500

    fix blogs 1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
-
+/** @type {import('next').NextConfig} */
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
     images: {
